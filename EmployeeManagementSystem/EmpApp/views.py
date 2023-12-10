d13b363695c316a7953ecbb864f3d5b344017e0f
one-numan <numamk786@gmail.com>
2023-12-10 22:06:46 +0530

    Created Routes CRUD | Only HTML CSS Files and CRUD Layout Builds

--- EmployeeManagementSystem/EmpApp/views.py
+++ EmployeeManagementSystem/EmpApp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Employee, Role, Department
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def all_emp(request):
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
+    }
+
+    print(context)
+
+    return render(request, 'all_emp.html', context)
+
+
+def add_emp(request):
+    return render(request, 'add_emp.html')
+
+
+def remove_emp(request):
+    return render(request, 'remove_emp.html')
+
+
+def filter_emp(request):
+    return render(request, 'filter_emp.html')
