040a057315e374c33764481e3bc8fa143eac48e3
one-numan <numamk786@gmail.com>
2023-12-21 23:03:42 +0530

    Bug Fix | Removed Used Files

--- dp/EmployeeManagementSystem/EmpApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Employee, Department, Role
-
-# Register your models here.
-admin.site.register(Employee)
-admin.site.register(Department)
-admin.site.register(Role)
--- dp/EmployeeManagementSystem/apiV1/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Employee2, Role2
-# Register your models here.
-
-admin.site.register(Employee2)
-admin.site.register(Role2)
